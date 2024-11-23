c001d70c4bb7699506e5e5c694dcab7e77137c1a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-23 09:24:03 -0500

    feat: +p6_cirrus_cloudtrail_logging_start

---
+++ lib/cloudtrail.sh
@@ -0,0 +1,15 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6_cirrus_cloudtrail_logging_start()
+#
+#>
+######################################################################
+p6_cirrus_cloudtrail_logging_start() {
+
+  local trail_arn=$(p6_aws_svc_cloudtrail_trail_arns)
+  p6_aws_svc_cloudtrail_trail_logging_start "$trail_arn"
+
+  p6_return_void
+}
