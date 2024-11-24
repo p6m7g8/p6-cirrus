bf082b368fcfc8bace37d5177587c2c567dd93d9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-24 12:23:47 -0500

    fix: skip making the key if I already have it

--- lib/ec2/keypair.sh
+++ lib/ec2/keypair.sh
@@ -18,11 +18,12 @@ p6_cirrus_ec2_keypair_import() {
     local priv_key_path="$prefix/$key_name"
     local pub_key_path="$prefix/$key_name.pub"
 
-    p6_ssh_key_make "$priv_key_path" >/dev/null
-
-    p6_aws_cli_cmd ec2 import-key-pair \
-        --key-name "$key_name" \
-        --public-key-material fileb://$pub_key_path >/dev/null
+    if ! p6_file_exists "$priv_key_path"; then
+        p6_ssh_key_make "$priv_key_path" >/dev/null
+        p6_aws_cli_cmd ec2 import-key-pair \
+            --key-name "$key_name" \
+            --public-key-material fileb://$pub_key_path >/dev/null
+    fi
 
     p6_return_str "$key_name"
 }
