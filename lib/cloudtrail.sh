bac4dfb7d5ce4d25ef352c1a3ee745ea9096efd5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-23 20:25:02 -0500

    feat: +p6_cirrus_cloudtrail_trail_delete

--- lib/cloudtrail.sh
+++ lib/cloudtrail.sh
@@ -13,3 +13,18 @@ p6_cirrus_cloudtrail_logging_start() {
 
   p6_return_void
 }
+
+######################################################################
+#<
+#
+# Function: p6_cirrus_cloudtrail_trail_delete()
+#
+#>
+######################################################################
+p6_cirrus_cloudtrail_trail_delete() {
+
+  local trail_arn=$(p6_aws_svc_cloudtrail_trail_arns)
+  p6_aws_svc_cloudtrail_trail_logging_start "$trail_arn"
+
+  p6_return_void
+}
